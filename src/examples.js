862a41cda1aa5d96c11f7de13b5e1b1c684b86f8
Fred Sauer <fredsa@google.com>
2026-02-16 21:24:30 -0800

    z80 hello.asm

--- src/examples.js
+++ src/examples.js
@@ -601,4 +601,30 @@ loop    ld      a,(hl)      ; Copy 1 source byte
         jp      nz,loop     ; repeat the loop
         pop     af          ; Restore AF
         ret                 ; Return
-        end`;
+        end
+
+; hello.asm
+;----------
+FOO         equ  6*7 + 1 ; 43
+BAR         equ  %1 << 8 ; 128
+
+CHAN_OPEN   equ  5633
+PRINT       equ  8252
+
+            org  0x5ccb
+loop
+            ld   a, 2                ; 3E 02
+            call CHAN_OPEN           ; CD 01 16
+            ld   de, text            ; 11 0E 7F
+            ld   bc, textend-text    ; 01 0E 00
+            call PRINT               ; C3 3C 20
+            jmp  loop
+
+text        defb 'Hello, World!'     ; 48 65 6C 6C 6F 2C 20 57
+                                     ; 6F 72 6C 64 21
+            defb 13                  ; 0D
+textend
+
+        org 0xff57
+        defb 00h
+`;
